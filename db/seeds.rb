93c5b4c785874225be97a17fed2cd4bcff595d7f
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-02-21 10:29:34 -0700

    Add last changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,9 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first)Chef
+
+
+
+Chef.create(name: "Mark")
+Dish.create(name: "steak fries", description: "delicious")
--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -10,18 +10,22 @@ RSpec.describe Chef, type: :model do
 
   describe "methods" do
     it "should return a list of ingredients used by the chef" do 
+      
       @chef = Chef.create!(name: "Alex")
+
       @dish1 = @chef.dishes.create(name: "steak fries", description: "Amazing")
       @ingredient1 = Ingredient.create!(name: "meat", calories: 400)
       @ingredient2 = Ingredient.create!(name: "fries", calories: 200)
       @ingredient3 = Ingredient.create!(name: "marshmallows", calories: 300)
       @dish1.ingredients << @ingredient1
       @dish1.ingredients << @ingredient2
+
       @dish2 = @chef.dishes.create(name: "steak carrots", description: "Amazing")
       @ingredient1 = Ingredient.create!(name: "meat", calories: 400)
       @ingredient3 = Ingredient.create!(name: "carrots", calories: 200)
       @dish2.ingredients << @ingredient1
       @dish2.ingredients << @ingredient3
+
       expect(@chef.used_ingredients).to eq(["carrots", "fries", "meat"])
     end
 
